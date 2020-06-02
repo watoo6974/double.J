9c1904c1cb18eb9ff04c17d9178a80d8c52cb258
double.J <66119161+watoo6974@users.noreply.github.com>
2020-06-02 17:07:14 +0900

    version 1.1

---
+++ script/drink.js
@@ -0,0 +1,24 @@
+function calcNow(){
+    calc.e.value = calc_divide(calc.d.value, calc.a.value)
+    var g = calc_minus(calc.a.value, calc.b.value)
+    var f = calc_minus(calc.c.value, calc.d.value)
+    var h = calc_divide(f, g)
+    calc.i.value = calc_plus(calc.e.value, h)
+
+}
+function calc_divide(d, a){
+    return d/a;
+}
+function calc_minus(a, b){
+    return a-b;
+}
+function calc_minus(c, d){
+    return c-d;
+}
+function calc_divide(f, g){
+    return f/g;
+}
+function calc_plus(e, h){
+    return Number(e) + Number(h);
+}
+
