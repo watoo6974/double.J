4ab63ea1fcb8740bbd51273436b21ec654f5d208
double.J <66119161+watoo6974@users.noreply.github.com>
2020-06-09 07:08:00 +0900

    Add files via upload

--- script/drink.js
+++ script/drink.js
@@ -4,7 +4,7 @@ function calcNow(){
     var f = calc_minus(calc.c.value, calc.d.value)
     var h = calc_divide(f, g)
     calc.i.value = calc_plus(calc.e.value, h)
-
+    
 }
 function calc_divide(d, a){
     return d/a;
@@ -25,8 +25,9 @@ function Reset(){
     document.resetbutton.reset();
 }
 
-Math.round(calc.i.value, calc.e.value)
+
 
 function drinkAlert(){
     alert ('인원 : 함께 즐긴 인원이 몇 명인지 입력합니다.\n비음주자 : 함께 한 인원들중 술을 안 드신 인원이 몇 명인지 입력합니다.\n총계 : 술과 안주를 포함한 모든 금액을 쓰시는 칸입니다. 단위는 원 입니다.\n안주 : 드신 것중 안주에 해당하는 금액만 쓰시면 됩니다.\n음주자금액 : 술과 안주를 함께 즐긴 분들이 내셔야 되는 금액 입니다.\n비음주자금액 : 술과 안주중 안주만 즐긴 분들이 내셔야 하는 금액 입니다.')
 }
+Math.round(calc.i.value, calc.e.value)
--- script/oil.js
+++ script/oil.js
@@ -5,7 +5,7 @@ function calcNow(){
     var x = calc_plus(z, calc.e.value)
     calc.f.value = calc_divide(x, calc.a.value)
 
-
+    
 }
 function calc_multiply(y, c){
     return y*c;
@@ -23,7 +23,8 @@ function calc_divide(x, a){
 function Reset(){
     document.resetbutton.reset();
 }
-Math.round(calc.f.value)
+
 function oilAlert(){
     alert ('인원 : 주행에 함께 한 인원이 몇 명인지 입력합니다.\n거리 : 함께 한 총 주행거리를 입력 하시면 됩니다.\n리터당기름값 : 자주 이용하는 주유소의 현재 금액을 쓰시면 됩니다.\n평균연비 : 주행 하시는 차량의 평균연비를 입력하시면 됩니다. 예를들어 1리터로 8.3km를 주행하신다면 8.3 을 입력하면 됩니다.\n톨비 : 통과하신 톨게이트의 비용을 쓰시면 됩니다.\n금액 : 주행에 함께한 모든 인원이 운전자에게 주는 금액입니다.')
 }
+Math.round(calc.f.value)
